77be1547875cdca48c295f48ec4b0b9c4ec9c2fc
Marc Prud'hommeaux <marc@skip.tools>
2026-08-19 13:30:02 -0400

    Change source.skip.tools references to github.com/skiptools (#22)

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
         .library(name: "SkipDevice", targets: ["SkipDevice"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.2.31"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "1.2.17"),
+        .package(url: "https://github.com/skiptools/skip.git", from: "1.2.31"),
+        .package(url: "https://github.com/skiptools/skip-foundation.git", from: "1.2.17"),
     ],
     targets: [
     .target(name: "SkipDevice", dependencies: [
@@ -24,7 +24,7 @@ let package = Package(
 )
 
 if Context.environment["SKIP_BRIDGE"] ?? "0" != "0" {
-    package.dependencies += [.package(url: "https://source.skip.tools/skip-bridge.git", "0.0.0"..<"2.0.0")]
+    package.dependencies += [.package(url: "https://github.com/skiptools/skip-bridge.git", "0.0.0"..<"2.0.0")]
     package.targets.forEach({ target in
         target.dependencies += [.product(name: "SkipBridge", package: "skip-bridge")]
         target.swiftSettings = (target.swiftSettings ?? []) + [.define("SKIP_BRIDGE")]
